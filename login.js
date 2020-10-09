209dcd94439f094742d882d16ec5c373b284b0f5
Ashwith Poojary <pashwith25@gmail.com>
2020-10-09 14:17:40 +0530

    javascript form validation 1

--- login.js
+++ login.js
@@ -12,5 +12,34 @@ signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
 
-// const name = document.getElementsByClassName('input_name')
-// if (name.
+function form_submit(){
+	if (formValidator()==true){
+		var forms = document.getElementsByName("signup_form");
+		forms[0].submit();
+		alert("Form submission succesfull!");
+	}
+}
+
+
+function formValidator(){
+	flag=false;
+	var username = document.forms["signup_form"]["name"].value;
+	var pass = document.getElementsByName("password");
+	var conf_pass = document.getElementsByName("confirm_password");
+	console.log(username);
+	console.log(pass);
+	if (username==""){
+		alert("Name is empty");
+	}
+	else if(pass[0].value=="" || conf_pass[0].value==""){
+		alert("Password fields are empty");
+	}
+	else if(pass[0].value != conf_pass[0].value){
+		alert("Passwords did not match! Please try again");
+		return false;
+	}
+	else{
+		flag=true;
+	}
+	return flag;
+}
